c74dff8677a662986fe43f7a5acfe4b863ea7783
tabakazu <tkazz.bd1@gmail.com>
2018-10-03 02:25:20 +0900

    #15 add auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/myself'
-
+  post '/auth/register'
+  post '/auth/authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
