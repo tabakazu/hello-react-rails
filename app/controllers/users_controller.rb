fed909dce20bde939ff84cbcd2146e8aafe65e47
tabakazu <tkazz.bd1@gmail.com>
2018-10-04 01:40:13 +0900

    19 add user show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
   before_action :authenticate, only: [:index]
+  before_action :set_user, only: [:show]
 
   # GET /users
   def index
     render json: @current_user
   end
+
+  # GET /users/1
+  def show
+    render json: @user.slice(:id, :email)
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      begin
+        @user = User.find(params[:id])
+      rescue
+        render json: { error: 'Not found'}, status: 404
+      end
+    end
 end
