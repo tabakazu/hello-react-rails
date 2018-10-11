9651b90de90d778782e5acfdb8098f4a320d9a85
tabakazu <tkazz.bd1@gmail.com>
2018-10-12 03:05:15 +0900

    #33 create follow model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :active_follows, class_name: 'Follow', foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_follows, class_name: 'Follow', foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_162803) do
+ActiveRecord::Schema.define(version: 2018_10_11_174223) do
+
+  create_table "follows", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+  end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
