314d390176388f33c8ec824a6ee0c21483f44b4f
tabakazu <tkazz.bd1@gmail.com>
2018-10-16 00:45:24 +0900

    #39 modify like action path

---
+++ app/controllers/like_controller.rb
@@ -0,0 +1,22 @@
+class LikeController < ApplicationController
+  before_action :authenticate, only: [:create, :destroy]
+
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    like = Like.new({
+      user_id: @current_user.id,
+      micropost_id: micropost.id
+    })
+    if like.save
+      render json: like, status: :created
+    else
+      render json: { errors: like.errors }
+    end
+  end
+
+  def destroy
+    like = @current_user.likes.find_by(micropost_id: params[:micropost_id])
+    like.destroy
+    render json: {}, status: :no_content
+  end
+end
