340f61bc419b564131f3d1c630eb668b395612c7
tabakazu <tkazz.bd1@gmail.com>
2018-10-16 01:03:35 +0900

    #40 modify follow action path

---
+++ app/controllers/follow_controller.rb
@@ -0,0 +1,22 @@
+class FollowController < ApplicationController
+  before_action :authenticate, only: [:create, :destroy]
+
+  def create
+    user = User.find(params[:followed_id])
+    follow = Follow.new({
+      follower_id: @current_user.id,
+      followed_id: user.id
+    })
+    if follow.save
+      render json: follow, status: :created
+    else
+      render json: { errors: follow.errors }
+    end
+  end
+
+  def destroy
+    follow = @current_user.active_follows.find_by(followed_id: params[:followed_id])
+    follow.destroy
+    render json: {}, status: :no_content
+  end
+end
