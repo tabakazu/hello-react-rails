a4134c450c8f45e369acb9783f1a8420d2e75bf7
tabakazu <tkazz.bd1@gmail.com>
2018-11-01 02:04:40 +0900

    #49 add *path line for react-router

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,9 @@ Rails.application.routes.draw do
         post 'register'
         post 'token'
         get 'user'
-      end  
+      end
     end
   end
+  match '*path', to: 'home#index', via: :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
