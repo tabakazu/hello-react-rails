62708dfcda8c5f6657612463c4dcf88df9893f7e
tabakazu <tkazz.bd1@gmail.com>
2018-11-13 03:15:43 +0900

    #69 add hello world of redux saga

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import createSagaMiddleware from 'redux-saga'
 import App from '../components/App'
 import IndexReducer from '../reducers'
+import IndexSagas from '../sagas'
 
-const store = createStore(IndexReducer)
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  IndexReducer,
+  applyMiddleware(sagaMiddleware)
+)
+sagaMiddleware.run(IndexSagas)
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
---
+++ app/javascript/sagas/index.js
@@ -0,0 +1,8 @@
+import fork from 'redux-saga/effects'
+import 'babel-polyfill'
+
+function* IndexSaga() {
+  console.log('Hello Sagas!')
+}
+
+export default IndexSaga
